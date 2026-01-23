0342f8d25abcee3ca0af990b705f8fbae6318a6e
JaeYun <sim_518@naver.com>
2026-01-23 17:39:45 +0900

    feat: header 로고 추가

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,12 @@
+import b_logLogo from '../assets/B_log.png'
+
+
 const Header = () => {
   return (
     <>
-      <header>헤더임</header>
+      <header className="w-100% h-10px fixed top-0">
+        <img src={b_logLogo} alt="B_log 로고" />
+      </header>
     </>
   );
 }
