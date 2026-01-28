3a34387047647daf61de47646f27c504aa235bfc
JaeYun <sim_518@naver.com>
2026-01-28 15:19:57 +0900

    header 크기, 위치 변경 크기: scale 70으로

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <header className="w-100% h-10px fixed top-0">
-        <img src={b_logLogo} alt="B_log 로고" />
+        <img src={b_logLogo} alt="B_log 로고" className="scale-70 ml-10 mt-2"/>
       </header>
     </>
   );
