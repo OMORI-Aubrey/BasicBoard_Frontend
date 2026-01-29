97a4aef94e812fa46316c29b98394877822b6e6c
JaeYun <sim_518@naver.com>
2026-01-29 18:45:12 +0900

    feat: 애러 404 페이지

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,20 @@
 const ErrorPage = () => {
   return (
     <>
-      애러나면 여기로 ㄱㄱ
+      <div
+        className="
+        w-full
+        h-screen
+        flex
+        justify-center
+        items-center
+        ">
+        
+        <img
+          src="/src/assets/errors/error404.png"
+          alt="404"
+        />
+      </div>
     </>
   );
 }
