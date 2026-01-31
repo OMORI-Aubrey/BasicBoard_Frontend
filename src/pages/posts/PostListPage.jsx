21dd6039e8c04e5d80d83edaaef9f6e86b81e9f6
JaeYun <sim_518@naver.com>
2026-01-31 17:05:39 +0900

    feat: 글 목록에서 게시글을 눌렀을때 다음 페이지로 이동

--- src/pages/posts/PostListPage.jsx
+++ src/pages/posts/PostListPage.jsx
@@ -99,23 +99,25 @@ const PostListPage = () => {
         <section className="h-screen">
 
           {posts.map(post => (
-            <article key={post.id} className="h-[20%] border-b-2 flex items-center border-gray-300/30">
-              <div className="ml-4 flex flex-col">
-                <h3 className="font-semibold mb-1">
-                  {post.title}
-                </h3>
-                <p className="font-light text-xs text-[#5D5F67] mb-5">
-                  {post.content}
-                </p>
-                <span
-                  className="text-xs font-light text-[#AAAAAA]"
-                >
-                  {new Date(post.createdAt).toLocaleString()}
-                </span>
-              </div>
-            </article>
+            <Link to={`/posts/${post.id}`}>
+              <article key={post.id} className="h-[20%] border-b-2 flex items-center border-gray-300/30">
+                <div className="ml-4 flex flex-col">
+                  <h3 className="font-semibold mb-1">
+                    {post.title}
+                  </h3>
+                  <p className="font-light text-xs text-[#5D5F67] mb-5">
+                    {post.content}
+                  </p>
+                  <span
+                    className="text-xs font-light text-[#AAAAAA]"
+                  >
+                    {new Date(post.createdAt).toLocaleString()}
+                  </span>
+                </div>
+              </article>
+            </Link>
           ))}
-          
+
         </section>
 
         {/* 페이지네이션 */}
@@ -137,7 +139,7 @@ const PostListPage = () => {
           <a href="#">
             <img src="/src/assets/icons/blackAngleBracket.svg" alt="꺽쇠" />
           </a> {/* 왼쪽 꺽쇠 */}
-          
+
           <a href="#" className="font-bold text-black">1</a> {/*현재 페이지일때는 text-bold, text-black */}
           <a href="#" className="">2</a>
           <a href="#" className="">3</a>
